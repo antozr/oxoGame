6954b202b8736bacc31240d4755f17878043dc40
anto__dev <antonidwm@gmail.com>
2022-06-16 11:59:39 +0100

    ADD choice machine analyse

--- game.py
+++ game.py
@@ -3,7 +3,7 @@
 
 from random import randint
 import time
-
+from verification_analyse import verification_analyse_causality
 print("""
                             $$\                                     $$\                      \n
                       $$ |                                    $$ |                     \n
@@ -23,6 +23,17 @@ tab_position_possible = [" " ," "," "," " ," " , " ", " ", " "," " ]
 tab_game_virgin = " __"+tab_position_possible[0]+"__|__"+tab_position_possible[1]+"__|__"+tab_position_possible[2]+"__"+"\n _____|_____|______ \n"+" __"+tab_position_possible[3]+"__|__"+tab_position_possible[4]+"__|__"+tab_position_possible[5]+"__"+"\n _____|_____|______\n"+" __"+tab_position_possible[6]+"__|__"+tab_position_possible[7]+"__|__"+tab_position_possible[8]+"__"
 
 
+new_choice = [1, 2, 5, 6, 7, 9]
+size_list = len(new_choice)-1
+print(size_list)
+#new_choice = new_choice[randint(0,len(new_choice)-1)]
+new_choice_machine = verification_analyse_causality(new_choice, 2)
+print(new_choice_machine)
+new_choice = new_choice_machine[randint(0, len(new_choice_machine) - 1)]
+print(new_choice)
+
+
+
 # choix de la couleur 
 choice_user_color = str(input("Choice the X or O for the game : "))
 
@@ -40,30 +51,182 @@ print("The choice of machine is : "+ choice_machine_color)
 
 #choix de la position 
 # print(len(tab_position_possible))
+finish_game = False
 count_round = 0
+print('Your possibility is between 1 and 9 \n')
 choice_user = int(input('Choice your position between the disponibility number : '))
 print("Your position is {}".format(choice_user))
 
+### création de la fonction pour le jeu 
 
 
-if count_round == 0 :
-    if choice_user >= 1 and choice_user <=9:
-        tab_position_possible[choice_user] = choice_user_color
-        #print(tab_position_possible[choice_user])
-        time.sleep(0.2)
-        choice_machine = randint(0,8)
-        if choice_machine == choice_user :
-            choice_machine = randint(0,8)
-        print('The position of the machine is : {} '.format(choice_machine + 1))
-        tab_position_possible[choice_machine] = choice_machine_color
-        count_round += 1
-        print(count_round)
-    else : 
-        print('The position dispinible is : '+len(tab_position_possible))
-        choice_user = int(input('Choice your position between the disponibility number : '))
-else : 
-    print('You past the second round !!!!')
+### vérification de la causalité avec les itérations en fuchier externe 
+# def verification_analyse_causality (new_choice, choice_user):
+#     print(new_choice)
+#     if choice_user in new_choice[0:2] :
+#         new_choice_machine = new_choice[:-4]
+#         return new_choice_machine
+#     elif choice_user in new_choice[2:3] :
+#         new_choice_machine = new_choice[0:1] or new_choice[4:5]
+#         return new_choice_machine
+#     elif len(new_choice) == 6 :
+#         if choice_user in new_choice[4:5] :
+#              new_choice_machine = new_choice[:4]
+#              return new_choice_machine
+#     else : 
+#         new_choice_machine = new_choice[randint(0, len(new_choice) - 1)]
+        
+        ## vérifier utilité et var 
+### vérification pour ''' l'ia ''' du robot 
+def verification_choice_machine ():
     
-#création du tableau à chaque tours 
-tab_game_virgin = " __"+tab_position_possible[0]+"__|__"+tab_position_possible[1]+"__|__"+tab_position_possible[2]+"__"+"\n _____|_____|______ \n"+" __"+tab_position_possible[3]+"__|__"+tab_position_possible[4]+"__|__"+tab_position_possible[5]+"__"+"\n _____|_____|______\n"+" __"+tab_position_possible[6]+"__|__"+tab_position_possible[7]+"__|__"+tab_position_possible[8]+"__"    
-print(tab_game_virgin )
+    if choice_machine == 1 :
+        new_choice = [2, 3, 4, 5, 7, 9]
+        new_choice_machine = verification_analyse_causality(new_choice,choice_user)
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice,choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    
+    elif choice_machine == 2 :
+        new_choice = [1, 3, 5, 8]
+        
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice,choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    elif choice_machine == 3 :
+        new_choice = [1, 2, 5, 6, 7, 9]
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice,choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    
+    elif choice_machine == 4 :
+        new_choice = [1, 5, 6, 7]
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice,choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    
+    elif choice_machine == 5 :
+        new_choice = [2, 4, 6, 8]
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice,choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    
+    elif choice_machine == 6 :
+        new_choice = [3, 4, 7, 9]
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice,choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    
+    elif choice_machine == 7 :
+        new_choice = [1, 3, 4, 5,8, 9]
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice,choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    
+    elif choice_machine == 8 :
+        new_choice = [2, 5,7, 9]
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice, choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    
+    elif choice_machine == 9 :
+        new_choice = [1, 3, 5, 6, 7, 8]
+        size_list = len(new_choice)-1
+        #new_choice = new_choice[randint(0,len(new_choice)-1)]
+        new_choice_machine = verification_analyse_causality(new_choice, choice_user)
+        new_choice = new_choice_machine[randint(0,size_list)]
+        if new_choice == choice_user :
+            new_choice = new_choice[randint(0,size_list)]
+        return new_choice
+    
+#########################################################################  game 
+while finish_game != True:
+    if count_round == 0 :
+        if choice_user >= 1 and choice_user <=9:
+            tab_position_possible[choice_user - 1] = choice_user_color
+            print(tab_position_possible[choice_user-1]) # vérification de la position problème avec la position 0/1
+            #machine time 
+            time.sleep(0.2)
+            choice_machine = randint(0,8)
+            if choice_machine == choice_user :
+                choice_machine = randint(0,8)
+            print('The position of the machine is : {} '.format(choice_machine + 1))
+            tab_position_possible[choice_machine] = choice_machine_color
+            count_round += 1
+            print(count_round)
+            ##
+            #création du tableau à chaque tours 
+            tab_game_virgin = " __"+tab_position_possible[0]+"__|__"+tab_position_possible[1]+"__|__"+tab_position_possible[2]+"__"+"\n _____|_____|______ \n"+" __"+tab_position_possible[3]+"__|__"+tab_position_possible[4]+"__|__"+tab_position_possible[5]+"__"+"\n _____|_____|______\n"+" __"+tab_position_possible[6]+"__|__"+tab_position_possible[7]+"__|__"+tab_position_possible[8]+"__"    
+            print(tab_game_virgin )
+        else : 
+            print('The position dispinible is : '+len(tab_position_possible))
+            choice_user = int(input('Choice your position between the disponibility number : '))
+            
+    elif count_round == 1 : 
+        choice_user = int(input('Choice your position between the disponibility number : '))
+        if choice_user >= 1 and choice_user <=9:
+            verification_choice_machine ()
+            tab_position_possible[choice_user - 1] = choice_user_color
+            #print(tab_position_possible[choice_user])
+            #machine time 
+            time.sleep(0.2)
+            # choice_machine = randint(0,8)
+            # if choice_machine == choice_user :
+            choice_machine = randint(0,8)
+            choice_machine = verification_choice_machine ()
+            print(choice_machine)
+            print('The position of the machine is : {} '.format(choice_machine + 1))
+            tab_position_possible[choice_machine] = choice_machine_color
+            #change round 
+            count_round += 1
+            print(count_round)
+            ##
+            #création du tableau à chaque tours 
+            tab_game_virgin = " __"+tab_position_possible[0]+"__|__"+tab_position_possible[1]+"__|__"+tab_position_possible[2]+"__"+"\n _____|_____|______ \n"+" __"+tab_position_possible[3]+"__|__"+tab_position_possible[4]+"__|__"+tab_position_possible[5]+"__"+"\n _____|_____|______\n"+" __"+tab_position_possible[6]+"__|__"+tab_position_possible[7]+"__|__"+tab_position_possible[8]+"__"    
+            print(tab_game_virgin )
+        else : 
+            print('The position dispinible is : '+len(tab_position_possible))
+            choice_user = int(input('Choice your position between the disponibility number : '))
+    else : 
+        print('You past the second round !!!!')
+        finish_game = True
+
+# #création du tableau à chaque tours 
+# tab_game_virgin = " __"+tab_position_possible[0]+"__|__"+tab_position_possible[1]+"__|__"+tab_position_possible[2]+"__"+"\n _____|_____|______ \n"+" __"+tab_position_possible[3]+"__|__"+tab_position_possible[4]+"__|__"+tab_position_possible[5]+"__"+"\n _____|_____|______\n"+" __"+tab_position_possible[6]+"__|__"+tab_position_possible[7]+"__|__"+tab_position_possible[8]+"__"    
+# print(tab_game_virgin )
---
+++ verification_analyse.py
@@ -0,0 +1,18 @@
+from random import randint
+
+def verification_analyse_causality (new_choice, choice_user): #choice user 
+    print(new_choice)
+    if int(choice_user) in new_choice[0:2] :
+        new_choice_machine = new_choice[:-4]
+        return new_choice_machine
+    elif choice_user in new_choice[2:3] :
+        new_choice_machine = new_choice[0:1] or new_choice[4:5]
+        return new_choice_machine
+    elif len(new_choice) == 6 :
+        if choice_user in new_choice[4:5] :
+             new_choice_machine = new_choice[:4]
+             return new_choice_machine
+    else : 
+        new_choice_machine = new_choice[randint(0, len(new_choice) - 1)]
+        
+        ## vérifier utilité et var 
